aa2de14c6c2bcfd72245406c12d3cc2c4cda893b
LarsPlayz <86638368+LarsPlayz@users.noreply.github.com>
2021-11-08 13:05:17 +0100

    hellowo
    very cool code

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,4 @@
-print("I am not a racist. I accept all races. Even the bad ones.")
+if knut:
+  print("I am a racist. I hate all races. Even the good ones.")
+else:
+  print("I am not a racist. I accept all races. Even the bad ones.")
